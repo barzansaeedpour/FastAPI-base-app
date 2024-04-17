3d16506f1b1b761075e7319f1cfe9db5dad7f8bd
barzansaeedpour <barzansaeedpour@gmail.com>
2024-04-17 12:08:51 +0330

    adding the db

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from fastapi import FastAPI
+from typing import List
+from models import User
+from uuid import UUID, uuid4
+from models import Role, Gender
+
 
 app = FastAPI()
+
+db: List[User] = [
+    
+    User(
+        id=uuid4(),
+        first_name="Barzan",
+        last_name="Saeedpour",
+        gender = Gender.male,
+        roles = [Role.admin],
+        ),
+    
+    User(
+        id=uuid4(),
+        first_name="parsa",
+        last_name="soli",
+        gender = Gender.male,
+        roles = [Role.student, Role.user],
+        ),
+]
 
 @app.get('/')
 def root():
